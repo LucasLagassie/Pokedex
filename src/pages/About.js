45e16fa78fcd0a45c06413dc8138e2df11554545
LucasLagassie <lucas.lagassie@oclock.school>
2024-06-29 16:57:46 +0200

    mise en place projet

---
+++ src/pages/About.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis,
+        doloremque odio iste iure officia ducimus alias delectus ad sapiente
+        dolore numquam dignissimos minus perferendis neque hic adipisci cumque
+        maxime tenetur, voluptatibus expedita quas! Ab cumque fugit fuga harum
+        temporibus, accusantium deserunt eum expedita voluptates nisi provident
+        ullam aperiam molestias laborum reprehenderit quidem nam itaque incidunt
+        earum ad et vero voluptatem quibusdam. Tenetur veniam error quasi
+        eligendi iusto reprehenderit laborum quas odit, minus omnis, similique
+        ipsam vero qui voluptates distinctio maiores, ab quaerat rerum
+        inventore! Voluptatum fugit corrupti consequuntur reiciendis non!
+        Possimus culpa reiciendis sint magnam minima aliquam aut nam sit.{" "}
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        officiis perspiciatis porro suscipit distinctio perferendis iste sint,
+        hic quod. Delectus, molestiae distinctio exercitationem odio autem, nemo
+        voluptatem quas iste aliquam officia sint suscipit minus eum alias, ipsa
+        sequi quae vero quibusdam veritatis. Laborum soluta distinctio incidunt
+        perspiciatis earum fuga aperiam.
+      </p>
+    </div>
+  );
+};
+
+export default About;
